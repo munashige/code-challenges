b1b39b3a1a60ee1ce7e806028ef0dda14db4adf8
Stanislav N <79140119+munashige@users.noreply.github.com>
2023-05-05 17:12:07 +0300

    Add files via upload

---
+++ code-wars/6-kyu/roman.encoder.js
@@ -0,0 +1,24 @@
+function solution(number){
+    const dict = [
+      [1000, "M"],
+      [900, "CM"],
+      [500, "D"],
+      [100, "C"],
+      [90, "XC"],
+      [50, "L"],
+      [10, "X"],
+      [9, "IX"],
+      [5, "V"],
+      [4, "IV"],
+      [1, "I"]
+    ]
+    
+    if(number === 0) {
+      return ""
+    } for(let i =0; i< dict.length; i++) {
+      if(number  >= dict[i][0]) {
+        return dict[i][1] + solution(number - dict[i][0])
+      }
+    }
+  }
+  
