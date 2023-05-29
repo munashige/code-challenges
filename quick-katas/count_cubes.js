1db9f21ae790baad2216d181f19a70ceb5e2f4f9
Stanislav N <79140119+munashige@users.noreply.github.com>
2023-05-29 19:06:49 +0300

    count cubes solution

---
+++ quick-katas/count_cubes.js
@@ -0,0 +1,12 @@
+/* Upon arriving at an interview, you are presented with a solid blue cube. 
+The cube is then dipped in red paint, coating the entire surface of the cube. 
+The interviewer then proceeds to cut through the cube in all three dimensions a certain number of times.
+Your function takes as parameter the number of times the cube has been cut. 
+You must return the number of smaller cubes created by the cuts that have at least one red face.
+*/
+
+function countSquares(cuts) {
+    return cuts == 0 ? 1 : 6 * cuts * cuts +2
+  }
+  
+  console.log(countSquares(0))
