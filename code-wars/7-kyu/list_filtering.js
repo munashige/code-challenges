676e0a645d08c45581105705f1a630b4ebfc7625
Stanislav N <79140119+munashige@users.noreply.github.com>
2023-06-11 20:41:46 +0300

    list filtering solution

---
+++ code-wars/7-kyu/list_filtering.js
@@ -0,0 +1,10 @@
+//In this kata you will create a function that takes a list of non-negative 
+//integers and strings and returns a new list with the strings filtered out.
+
+function filter_list(list) {
+    return list.filter(el => typeof el == "number")
+}
+
+
+console.log(filter_list([1,2,"3", 4, "abc"]))
+
