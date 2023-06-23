f40383569be5437b2be0a6e7de645d4fa52ceaa7
Stanislav N <79140119+munashige@users.noreply.github.com>
2023-06-23 22:20:21 +0300

    count the digit solution

---
+++ code-wars/7-kyu/count_the_digit.js
@@ -0,0 +1,15 @@
+/*
+Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+Square all numbers k (0 <= k <= n) between 0 and n.
+Count the numbers of digits d used in the writing of all the k**2.
+Implement the function taking n and d as parameters and returning this count.
+*/
+
+function nbDig(n,d) {
+    let res = 0
+    for(let i = 0; i<= n; i++) {
+        res += (i*i +"").split("").filter(el=> el ==d ).length
+    }
+    return res
+}
+
